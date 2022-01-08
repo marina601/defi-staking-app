3a568c79d01afddd4eef24bff8213cefc237054c
marina601 <marina601@live.com>
2022-01-08 17:20:23 +0100

    Initialized tether contract

---
+++ src/contracts/Tether.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.11;
+
+contract Tether {
+    string public name = 'Tether';
+    string public symbol = 'USD';
+    uint256 totalSupply = 1000000000000000000000; // 1 million tokens
+    uint8 decimals = 18;
+}
