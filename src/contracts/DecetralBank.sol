7c19f23a6816e92b1d5cdfcedba22b0eee6d326f
marina601 <marina601@live.com>
2022-01-10 14:05:16 +0100

    re runned migrations

--- src/contracts/DecetralBank.sol
+++ src/contracts/DecetralBank.sol
@@ -1,8 +1,17 @@
 pragma solidity ^0.5.16;
 
+import './RWD.sol';
+import './Tether.sol';
+
 // creating a contract for migrations
 contract DecentralBank {
     string public name = 'Decebtral Bank';
     address public owner;
-    
+    Tether public tether;
+    RWD public rwd;
+
+    constructor(RWD _rwd, Tether _tether) public {
+        rwd = _rwd;
+        tether = _tether;
+    }
 }
--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.16;
 contract Tether {
     string public name = 'Tether';
     string public symbol = 'USD';
-    uint256 totalSupply = 1000000000000000000000; // 1 million tokens
+    uint256 totalSupply = 1000000000000000000000000000; // 1 million tokens
     uint8 decimals = 18;
 
     event Transfer(
