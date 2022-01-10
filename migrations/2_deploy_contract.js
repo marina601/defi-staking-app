0ad5d90d8d8c9d7dae5fd91fdc9a5d707545db42
marina601 <marina601@live.com>
2022-01-10 12:41:22 +0100

    Runned new migrations

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,11 +1,18 @@
 // import the Tether file
 const Tether = artifacts.require('./Tether.sol');
-const Polkadot = artifacts.require('./Polkadot.sol');
+const RWD = artifacts.require('./RWD.sol');
+const DecentralBank = artifacts.require('./DecentralBank.sol');
+
 // create async function which will act as time delay
 module.exports = async function(deployer) {
   
     // Deploy Mock Tether Token
     await deployer.deploy(Tether)
-    await deployer.deploy(Polkadot)
+
+     // Deploy RWD
+    await deployer.deploy(RWD)
+
+      // Deploy Decentral Bank
+    await deployer.deploy(DecentralBank)
 
   }
