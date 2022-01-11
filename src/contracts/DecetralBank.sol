5b7dee28c054110ebd93b2be5737e6b035cf78f2
marina601 <marina601@live.com>
2022-01-11 11:57:30 +0100

    added staking functionality

--- src/contracts/DecetralBank.sol
+++ src/contracts/DecetralBank.sol
@@ -10,8 +10,36 @@ contract DecentralBank {
     Tether public tether;
     RWD public rwd;
 
+    address[] public stakers;
+
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     constructor(RWD _rwd, Tether _tether) public {
         rwd = _rwd;
         tether = _tether;
     }
+    
+    //staking function
+    function depositTokens(uint _amount) public {
+
+        //require staking amount to be greater then 0
+        require(_amount > 0, 'amount cannot be 0')
+        
+        // transfer tether tokens to the contract address for staking
+        tether.tranferFrom((msg.sender, address(this), _amount);
+
+    //Update staking balance
+    stakingBalance(msg.sender) = stakingBalance[msg.sender] + _amount;
+
+    if(!hasStaked) {
+        stakers.push[msg.sender];
+    }
+
+    //Update staking balance
+    isStaking[msg.sender] = true;
+    hasStaked[msg.sender] = true;
+    }
+
 }
