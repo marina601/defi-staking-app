8b4a6087a7c8b4b3c2dc298542879cccb8ed8767
marina601 <marina601@live.com>
2022-01-12 12:19:19 +0100

    Updated gitignore

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line
+import React, {Component} from "react";
+// eslint-disable-next-line
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className='text-center'>
+                <h1>Hello World!</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
