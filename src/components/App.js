0f1969644633a488d31e6b1f35b3a4883dc9d5d0
marina601 <marina601@live.com>
2022-01-13 13:19:49 +0100

    Initialized the state of the account

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,18 @@ import Navbar from "./Navbar";
 // eslint-disable-next-line
 
 class App extends React.Component {
+    // initialiaze the state of the account
+    constructor(props){
+        super(props)
+        this.state = {
+            account: '0xx0',
+        }
+    }
+
     render() {
         return (
             <div>
-                <Navbar></Navbar>
+                <Navbar account={this.state.account}></Navbar>
             </div>
         )
     }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ class Navbar extends Component {
             </h5>
             <ul className="navbar-nav px-3">
               <li className="text-nowrap d-none nav-item d-sm-none d-sm-block">
-                <small style={{ color: "white" }}>Account Number</small>
+                <small style={{ color: "white" }}>ACCOUNT NUMBER: {this.props.account}</small>
               </li>
             </ul>
           </nav>
