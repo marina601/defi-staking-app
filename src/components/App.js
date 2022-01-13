a0888ccf7c34a2120a3221572cb293d5dc309b74
marina601 <marina601@live.com>
2022-01-13 13:44:23 +0100

    Enabled web3 and connected to the meta mask

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,35 @@
 import React, {Component} from "react";
 import Navbar from "./Navbar";
 // eslint-disable-next-line
+import Web3 from "web3";
 
 class App extends React.Component {
+
+    async UNSAFE_componentWillMount() {
+        await this.loadWeb3()
+        await this.loadBlockchainData()
+    }
+
+    async loadWeb3() {
+        // enable web3 and connect to MetaMask
+        if(window.ethereuem) {
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereuem.enable()
+        } else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+            } else {
+                window.alert('No ethereum browser is detected: You can checkout MetaMask')
+            }
+        }
+    
+    async loadBlockchainData() {
+        const web3 = window.web3
+        const account = await web3.eth.getAccounts()
+        console.log(account)
+    }
+
     // initialiaze the state of the account
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             account: '0xx0',
