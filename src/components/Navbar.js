42faaa799c9404fd245763554a9b53a8f09e7386
marina601 <marina601@live.com>
2022-01-13 11:38:44 +0100

    Added a navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import bank from '../bank.png'
+
+class Navbar extends Component {
+    render() {
+        return (
+          <nav
+            className="navbar navbar-dart fixed-top shadow p-0"
+            style={{ backgroundColor: "black", height: "50px" }}
+          >
+            <h5 className='navbar-brand col-sm-3 col-md-2 mr-0' style={{ color: "white" }}>
+              <img src={bank} width='50' height='30' className="d-inline-block align-top" alt="logo of the bank" />
+              &nbsp; DAPP Yield Staking (Decentralized Banking)
+            </h5>
+            <ul className="navbar-nav px-3">
+              <li className="text-nowrap d-none nav-item d-sm-none d-sm-block">
+                <small style={{ color: "white" }}>Account Number</small>
+              </li>
+            </ul>
+          </nav>
+        );
+    }
+}
+
+export default Navbar
